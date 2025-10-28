4dee5110ce8398a9a0f66fa0904c35ce648ffe10
SoYoungLEE-me <lsy000626@gmail.com>
2025-10-29 03:46:42 +0900

    json 서버 세팅 완료

--- src/page/ProductAll.jsx
+++ src/page/ProductAll.jsx
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const ProductAll = () => {
+  const [productList, setProductList] = useState([]);
+
+  const getProducts = async () => {
+    const url = `http://localhost:5000/products`;
+    const res = await fetch(url);
+    const data = await res.json();
+    setProductList(data);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
     <div>
       <h1>전체 상품 페이지</h1>
